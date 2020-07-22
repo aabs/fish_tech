81bab1d37f5f9d8688c30b40eb52e4a55e3a7b49
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-22 19:41:18 +1000

    removed

--- functions/__fd2_tech_open.fish
+++ functions/__fd2_tech_open.fish
@@ -1,7 +1,6 @@
 function __fd2_tech_open -d "rhubarbs the blah"
   __fd2_select_item (find $fd2_tech_home -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 basename -a | string replace -a " " "_"  | string replace -a "-" "_" )
   if test $status -eq 0
-    echo selected $fd2_selected_item
     __fd2_tech_cd $fd2_selected_item
   end
 end
