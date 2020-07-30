3c1409af26c8079fd440e2736b63e22586b5789d
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-31 09:28:17 +1000

    updated to new subcmd std

--- conf.d/fish_tech.fish
+++ conf.d/fish_tech.fish
@@ -13,9 +13,9 @@
 #   set -U fd2_current_tmux_session "$fish_projects_current_sn"
 # end
 
-fd2_define_command tech "commands for working on file hierarchies divided by technologies"
+fd2_define_command -p tech -d "commands for working on file hierarchies divided by technologies"
 
-fd2_define_subcommand tech ls __fd2_tech_ls "list available technology areas"
-fd2_define_subcommand tech path __fd2_tech_path "get path to a specific area"
-fd2_define_subcommand tech cd __fd2_tech_cd "change dir to a chosen technology area"
-fd2_define_subcommand tech open __fd2_tech_open "select a tech area and cd to it"
+fd2_define_subcommand -p tech -c ls -f __fd2_tech_ls -d "list available technology areas"
+fd2_define_subcommand -p tech -c path -f __fd2_tech_path -d "get path to a specific area"
+fd2_define_subcommand -p tech -c cd -f __fd2_tech_cd -d "change dir to a chosen technology area"
+fd2_define_subcommand -p tech -c open -f __fd2_tech_open -d "select a tech area and cd to it"
