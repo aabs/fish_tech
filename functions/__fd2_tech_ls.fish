646dca90c71808884ae34a426f7d2db0c1e85d54
Andrew Matthews <matthews.andrew@gmail.com>
2020-10-11 18:03:18 +1100

    wip

--- functions/__fd2_tech_ls.fish
+++ functions/__fd2_tech_ls.fish
@@ -1,7 +1,7 @@
 function __fd2_tech_ls -d "rhubarbs the blah"
-set -l tech_roots (find $fd2_tech_home -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 basename -a | string replace -a " " "_"  | string replace -a "-" "_" )
+    set -l tech_roots (find $fd2_tech_home -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 basename -a | string replace -a " " "_"  | string replace -a "-" "_" )
 
-  for tr in $tech_roots
-      echo $tr
-  end 
+    for tr in $tech_roots
+        echo $tr
+    end
 end
--- functions/__fd2_tech_open.fish
+++ functions/__fd2_tech_open.fish
@@ -1,8 +1,8 @@
 function __fd2_tech_open -d "rhubarbs the blah"
-  __fd2_select_item (find $fd2_tech_home -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 basename -a | string replace -a " " "_"  | string replace -a "-" "_" )
-  if test $status -eq 0
-    __fd2_tech_cd $fd2_selected_item
-  end
+    fd2_select_item (find $fd2_tech_home -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 basename -a | string replace -a " " "_"  | string replace -a "-" "_" )
+    if test $status -eq 0
+        __fd2_tech_cd $fd2_selected_item
+    end
 end
 
 #  vim: set ts=2 sw=2 tw=80 et foldmethod=syntax foldlevelstart=20 :
